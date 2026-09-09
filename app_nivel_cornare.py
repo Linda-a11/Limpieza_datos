71c71d2a1a71612c2c351937c13ad67e7d30696b
Linda-a11 <110931109+Linda-a11@users.noreply.github.com>
2026-09-09 17:26:25 -0500

    Rename input fields for clarity in sidebar

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -106,10 +106,10 @@ def calcular_indice_calidad(df):
 # Sidebar — Parámetros de consulta y Umbrales Ambientales
 # ------------------------------------------------------------------
 st.sidebar.header("📋 Parámetros de Consulta")
-nombre_estudiante = st.sidebar.text_input("Nombre del estudiante", "Linda Maria Perez Regino")
+nombre_estudiante = st.sidebar.text_input("Nombre del Consultante", "Linda Maria Perez Regino")
 codigo_estacion = st.sidebar.text_input("Código de estación", "42")
-fecha_desde = st.sidebar.date_input("Desde", pd.to_datetime("2026-08-23")).strftime("%Y-%m-%d")
-fecha_hasta = st.sidebar.date_input("Hasta", pd.to_datetime("2026-08-30")).strftime("%Y-%m-%d")
+fecha_desde = st.sidebar.date_input("Fecha Inicial ", pd.to_datetime("2026-08-23")).strftime("%Y-%m-%d")
+fecha_hasta = st.sidebar.date_input("fecha final ", pd.to_datetime("2026-08-30")).strftime("%Y-%m-%d")
 calidad = st.sidebar.selectbox("Calidad", [1, 0], index=0, help="1 = solo datos validados")
 
 st.sidebar.markdown("---")
